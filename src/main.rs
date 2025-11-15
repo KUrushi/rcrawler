3b6484060df5dadf585cc2feca732a969fdf3f6a
KUrushi <delver.rasping_08@icloud.com>
2025-11-15 22:58:27 +0900

    URLからコンテンツを取得して、全てのリンクを取得する

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use scraper::{Html, Selector};
+use reqwest::Url;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 1. 対象のURL
-    let url = "https://www.rust-lang.org";
+    let url = Url::parse("https://www.rust-lang.org")?;
 
-    println!("Fetching: {url}");
+    println!("Fetching: {}", url.as_str());
 
     // 2. HTMLを取得
     // reqwest::get でアクセスし、`.text().await`でHTMLの文字列を取得する
-    let body = reqwest::get(url).await?.text().await?;
+    let body = reqwest::get(url.as_str()).await?.text().await?;
 
     // 3. HTMLを解析可能な形式に変換 (Parse)
     let document = Html::parse_document(&body);
@@ -18,7 +19,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     for element in document.select(&selector) {
         if let Some(link) = element.value().attr("href") {
-            println!("Link: {link}");
+            // url.joinは引数が完全なURLの場合には引数のURLが返ってくる
+            match url.join(link) {
+                Ok(link) => {
+                    println!("Getting Link: {}", link.as_str());
+                }
+                Err(e) => {
+                    eprintln!("Failed to get link: {}", e);
+                }
+            }
         } else {
             println!("Link is not found");
         }

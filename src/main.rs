6e95a6a6694981ed61357a4767a4264c2c614c30
KUrushi <delver.rasping_08@icloud.com>
2025-12-11 11:19:27 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::{Parser, Subcommand};
 use std::collections::HashSet;
 use reqwest::Url;
+use serde::{Deserialize, Serialize};
 use tokio::sync::mpsc;
 use my_crawler::extract_links;
 use tracing::{error, info};
 
+
+#[derive(Serialize, Deserialize)]
 struct CrawlResult {
     url: Url,
     links: Vec<Url>
@@ -60,46 +63,85 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Args::parse();
 
-    // 1. 対象のURL
-    let start_url = Url::parse(&args.url)?;
-    let mut known_urls = HashSet::<Url>::new();
-    known_urls.insert(start_url.clone());
+    let db = sled::open("data")?;
+    match args.command {
+        Commands::Crawl {url, tasks} => {
+            info!("Starting crawling for {} with {} tasks...", url, tasks);
+            let start_url = Url::parse(&url)?;
+            let mut known_urls = HashSet::<Url>::new();
+            known_urls.insert(start_url.clone());
 
-    // 1. チャネルを作成 (tx: 送信機、 rx: 受信機)
-    // バッファサイズは32
-    let (tx, mut rx) = mpsc::channel(args.tasks);
-    let mut active_tasks = 1_usize;
+            let (tx, mut rx) = mpsc::channel(tasks);
+            let mut active_tasks = 1_usize;
 
-    // 2. 最初のタスクを起動
-    // txを複製して、workerに持たせる
-    let first_tx = tx.clone();
+            let first_tx = tx.clone();
 
-    tokio::spawn(async move {
-        crawl_worker(start_url, first_tx).await;
-    });
+            tokio::spawn(async move {
+                crawl_worker(start_url, first_tx).await;
+            });
 
-    info!("Starting crawling concurrently");
 
-    while let Some(result) = rx.recv().await {
-        active_tasks -= 1;
-        info!("Finished: {} (Found {} links)", result.url, result.links.len());
-        
-        for link in result.links {
-            if let (Some(src), Some(tgt)) = (result.url.host(), link.host()) {
-                if src == tgt && known_urls.insert(link.clone()) {
-                    active_tasks += 1;
-                    let tx_clone = tx.clone();
-                    tokio::spawn(async move {
-                        crawl_worker(link, tx_clone).await;
-                    });
+            while let Some(result) = rx.recv().await {
+                active_tasks -= 1;
+                info!("Finished: {} (Found {} links)", result.url, result.links.len());
+
+                let json_bytes = serde_json::to_vec(&result)?;
+                let _ = db.insert(result.url.as_str(), json_bytes);
+
+
+                for link in result.links {
+                    if let (Some(src), Some(tgt)) = (result.url.host(), link.host()) {
+                        if src == tgt && known_urls.insert(link.clone()) {
+                            active_tasks += 1;
+                            let tx_clone = tx.clone();
+                            tokio::spawn(async move {
+                                crawl_worker(link, tx_clone).await;
+                            });
+                        }
+                    }
+                }
+
+                if active_tasks == 0 {
+                    break;
                 }
             }
         }
+        Commands::List => {
+            println!("Listing all data...");
+            let urls = db
+                .iter()
+                .filter_map(|x| {
+                if let Ok((key, v)) = x {
+                    if let Ok(byte_url) = std::str::from_utf8(&key){
+                        return Some(byte_url.to_string());
+                    }
+                }
+                None
+            }).collect::<Vec<String>>();
+            for url in urls {
+                println!("{}", url);
+            }
+        }
+        Commands::Get {key} => {
+            match db.get(key.as_str()) ? {
+                Some(bytes) => {
+                    let data: CrawlResult = serde_json::from_slice(&bytes)?;
 
-        if active_tasks == 0 {
-            break;
+                    info!("Data found for: {}", key);
+                    println!("URL:{}", data.url);
+                    println!("Links found: {}", data.links.len());
+
+                    for link in data.links.iter().take(5){
+                        println!(" - {}", link);
+                    }
+                }
+                None => {
+                    println!("{}のデータは見つかりませんでした。", key);
+                }
+            }
         }
     }
+
     Ok(())
 }
 
